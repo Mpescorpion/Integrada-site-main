a5898eed88c163d8ca015b7eb27873fd0dc1cc66
Mpescorpion <manuelescorpion3@gmail.com>
2024-11-28 11:38:18 -0300

    Update humanas.js

--- humanas.js
+++ humanas.js
@@ -1,1 +1,77 @@
+'use strict';
+
+// Configuração do canvas
+const canvas = document.getElementById('animation-background');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+});
+
+let particlesArray;
+
+// Classe de Partículas
+class Particle {
+    constructor(x, y, size, speedX, speedY) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // Reposiciona partículas ao sair da tela
+        if (this.x > canvas.width || this.x < 0 || this.y > canvas.height || this.y < 0) {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = '#FFD700'; // Cor dourada
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+// Inicializa as partículas
+function init() {
+    particlesArray = [];
+    const numberOfParticles = 100;
+
+    for (let i = 0; i < numberOfParticles; i++) {
+        const size = Math.random() * 3 + 1;
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const speedX = (Math.random() - 0.5) * 2;
+        const speedY = (Math.random() - 0.5) * 2;
+
+        particlesArray.push(new Particle(x, y, size, speedX, speedY));
+    }
+}
+
+// Animação
+function animate() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    particlesArray.forEach((particle) => {
+        particle.update();
+        particle.draw();
+    });
+
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
 
